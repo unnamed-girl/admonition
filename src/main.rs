ab51da6265affbe890ff37574c2d42580cbcb127
unnamed-girl <unnamedgirl.public@gmail.com>
2025-01-16 12:02:13 +0800

    Unconst things that should not be const

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,28 @@ use std::{fs, path::Path, process::exit};
 
 const ALPHABET:&str = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const TARGET: &str = "a1525fd30074da00e777a55e07107910";
-const GUESS: &str = "FIND US ALIVE";
-
-const NUM_WORDS: usize = 3;
-const WORDS:[&str; NUM_WORDS] = ["FIND", "US", "ALIVE"];
-
 fn main() {
-    let mut guess: String = GUESS.to_string();
-    mess_with_chars(&mut guess, 2, &check_correct);
+    let target = "a1525fd30074da00e777a55e07107910";
+    let guess = "FIND US ALIVE";
+    
+    let callback = |to_test: &str| check_correct(to_test, target);
+
+    mess_with_chars(&mut guess.to_string(), 2, &callback);
 
     let dict = read_lines_uppercase("words.txt");
-    substitute_words(&dict, 1, WORDS);
+    let mut words = guess.split_ascii_whitespace().collect();
+    substitute_words(&dict, 1, &mut words, &callback);
 }
 
-fn check_correct(input: &String) {
-    let hashed = format!("{:?}", md5::compute(input));
-    if TARGET == hashed.as_str() {
-        println!("{input}");
+fn check_correct(to_test: &str, target: &str) {
+    let hashed = format!("{:?}", md5::compute(to_test));
+    if target == hashed.as_str() {
+        println!("{to_test}");
         exit(0);
     }
 }
 
-fn mess_with_chars(input: &mut String, depth:u8, callback: &dyn Fn(&String)) {
+fn mess_with_chars(input: &mut String, depth:u8, callback: &dyn Fn(&str)) {
     for i in 0..input.chars().count() {
         let initial = input.chars().nth(i).unwrap();
         for char in ALPHABET.chars() {
@@ -53,21 +52,18 @@ fn read_lines_uppercase<P: AsRef<Path>>(path:P) -> Vec<String> {
     contents.lines().filter(|s| !s.contains("#!")).map(|str| str.to_uppercase()).collect()
 }
 
-fn substitute_words(dictionary: &Vec<String>, depth:u8, input: [&str; NUM_WORDS]) {
-    for i in 0..NUM_WORDS {
-        let mut temp = input.clone();
+fn substitute_words<'a>(dictionary: &'a Vec<String>, depth:u8, words: &mut Vec<&'a str>, callback: &dyn Fn(&str)) {
+    for i in 0..words.len() {
+        let initial = words[i];
         for word in dictionary {
-            temp[i] = word.as_str();
+            words[i] = word.as_str();
             if depth - 1 == 0 {
-                check_words(&temp);
+                callback(&words.join(" "));
             } else {
-                substitute_words(dictionary, depth - 1, temp);
+                substitute_words(dictionary, depth - 1, words, callback);
             }
         }
+        // undo changes
+        words[i] = initial;
     }
-}
-
-fn check_words(input: &[&str; NUM_WORDS]) {
-    let pattern = input.join(" ");
-    check_correct(&pattern);
 }
